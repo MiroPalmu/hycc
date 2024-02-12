ed8660c8d478804e4197197dda159f92caac894f
Miro Palmu <miro.palmu@helsinki.fi>
2024-02-12 18:27:35 +0200

    feat(parser): add parser_t

---
+++ include/hycc/parser.hpp
@@ -0,0 +1,91 @@
+#pragma once
+
+/// @file Includes the implementation of the parser.
+
+#include <algorithm>
+#include <exception>
+#include <optional>
+#include <ranges>
+#include <span>
+#include <string>
+#include <tuple>
+#include <vector>
+
+#include "hycc/tokenizer.hpp"
+
+namespace hycc {
+
+struct token_pattern {
+    token_type type;
+    std::u8string_view sv;
+
+    [[nodiscard]] constexpr auto match(const token& t) const {
+        return type == t.type and sv == t.sv_in_source;
+    }
+    [[nodiscard]] friend constexpr bool operator==(const token_pattern&,
+                                                   const token_pattern&) = default;
+};
+
+class parser_t {
+    std::span<token const> tokens_;
+    std::size_t next_unparsed_index_{ 0 };
+
+    [[nodiscard]] constexpr auto tokens_left() { return tokens_.size() - next_unparsed_index_; }
+    [[nodiscard]] constexpr decltype(auto) get_unparsed_tokens() {
+        return tokens_.subspan(next_unparsed_index_);
+    }
+
+  public:
+    [[nodiscard]] parser_t(const std::span<token const> tokens) : tokens_{ tokens } {}
+
+    [[nodiscard]] constexpr auto match_and_consume(const std::span<token_pattern const> pattern,
+                                                   const bool skip_whitespace = true)
+        -> std::optional<std::vector<token>> {
+        if (pattern.empty()) return std::vector<token>{};
+
+        auto skip = [&](const token& t) {
+            return skip_whitespace and t.type == token_type::whitespace;
+        };
+
+        namespace rv = std::ranges::views;
+        auto potential_matches_with_indecies =
+            get_unparsed_tokens() | rv::enumerate | rv::filter([&](const auto& x) {
+                const auto& [_, t] = x;
+                if (skip(t)) return false;
+                return true;
+            });
+
+        auto matched_tokens_with_indecies = rv::zip(pattern, potential_matches_with_indecies)
+                                            | rv::filter([](const auto& x) {
+                                                  const auto& [p, i_t] = x;
+                                                  const auto& [_, t]   = i_t;
+                                                  return p.match(t);
+                                              })
+                                            | rv::transform([](const auto& x) {
+                                                  const auto& [_, i_t] = x;
+                                                  return i_t;
+                                              });
+
+        using fold_t = std::tuple<std::vector<token>,
+                                  std::ranges::range_difference_t<decltype(get_unparsed_tokens())>>;
+        // Appends matched tokens together and take index of x unconditionally,
+        // so afther the fold it will contain the last matched index.
+        auto fold_f = [](fold_t lhs, const auto& x) {
+            const auto& [i, t] = x;
+            std::get<0>(lhs).push_back(t);
+            std::get<1>(lhs) = i;
+            return lhs;
+        };
+        auto [matched_tokens, last_index] =
+            std::ranges::fold_left(matched_tokens_with_indecies, fold_t{}, fold_f);
+
+        if (matched_tokens.size() == pattern.size()) {
+            // All matched, so they can be consumed!
+            next_unparsed_index_ += static_cast<std::size_t>(last_index) + 1;
+            return matched_tokens;
+        }
+        return {};
+    }
+};
+
+} // namespace hycc
---
+++ tests/test_parser.cpp
@@ -0,0 +1,225 @@
+#include <boost/ut.hpp> // import boost.ut;
+
+#include <algorithm>
+#include <format>
+#include <ranges>
+#include <source_location>
+#include <string>
+#include <tuple>
+#include <vector>
+
+#include "hycc/parser.hpp"
+#include "hycc/tokenizer.hpp"
+
+void expect_token(const hycc::token_pattern& expected,
+                  const hycc::token& got,
+                  const std::source_location loc = std::source_location::current()) {
+    const auto loc_info     = std::format("[actually at: {}]\n\n\t", loc.line());
+    const auto error_end    = std::format("\n");
+    const auto expected_str = std::string{ expected.sv.begin(), expected.sv.end() };
+    const auto got_str      = std::string{ got.sv_in_source.begin(), got.sv_in_source.end() };
+
+    boost::ut::expect(expected.type == got.type)
+        << loc_info
+        << std::format("expected hycc::token_type {}, got {} for: {}",
+                       hycc::get_name(expected.type),
+                       hycc::get_name(got.type),
+                       got_str)
+        << error_end;
+
+    boost::ut::expect(expected.sv == got.sv_in_source)
+        << loc_info << std::format("expected: {}, got {}", expected_str, got_str) << error_end;
+}
+
+void expect_tokens(const std::span<hycc::token_pattern const> expected,
+                   const std::span<hycc::token const> got,
+                   const std::source_location loc = std::source_location::current()) {
+    const auto got_str = std::ranges::fold_left_first(
+        got | std::views::transform([](const hycc::token& x) {
+            return std::string{ x.sv_in_source.begin(), x.sv_in_source.end() };
+        }),
+        [](std::string lhs, std::string rhs) { return lhs += "\n" + rhs; });
+
+    boost::ut::expect(expected.size() == got.size())
+        << std::format("[actually at: {}]\n\n\t", loc.line())
+        << std::format("expected {} tokens, got {}:\n{}",
+                       expected.size(),
+                       got.size(),
+                       got_str.value_or("[no tokens]"));
+
+    std::ranges::for_each(std::views::zip(expected, got),
+                          [](const auto& x) { expect_token(std::get<0>(x), std::get<1>(x)); });
+}
+
+int main() {
+    using namespace boost::ut;
+    using namespace hycc;
+
+    "parser_t can be constructed"_test = [] {
+        auto source       = source_code{ u8"123" };
+        const auto tokens = tokenize(source);
+        expect(nothrow([&] { [[maybe_unused]] auto _ = parser_t{ tokens }; }));
+    };
+
+    "parser_t can match patterns"_test = [] {
+        auto source        = source_code{ u8"123" };
+        const auto tokens  = tokenize(source);
+        auto parser        = parser_t{ tokens };
+        const auto pattern = std::vector<token_pattern>{ { token_type::integer, u8"123" } };
+        const auto matched = parser.match_and_consume(pattern);
+        expect(matched.has_value());
+        expect_tokens(pattern, matched.value());
+    };
+
+    "parser_t can consumes matched tokens"_test = [] {
+        auto source         = source_code{ u8"123" };
+        const auto tokens   = tokenize(source);
+        auto parser         = parser_t{ tokens };
+        const auto pattern  = std::vector<token_pattern>{ { token_type::integer, u8"123" } };
+        const auto matched1 = parser.match_and_consume(pattern);
+        expect(matched1.has_value());
+        expect_tokens(pattern, matched1.value());
+        const auto matched2 = parser.match_and_consume(pattern);
+        expect(not matched2.has_value());
+    };
+
+    "parser_t can match patterns ignoring whitespace"_test = [] {
+        auto source        = source_code{ u8" 123 " };
+        const auto tokens  = tokenize(source);
+        auto parser        = parser_t{ tokens };
+        const auto pattern = std::vector<token_pattern>{ { token_type::integer, u8"123" } };
+        const auto matched = parser.match_and_consume(pattern);
+        expect(matched.has_value());
+        expect_tokens(pattern, matched.value());
+    };
+
+    "parser_t can match whitespace"_test = [] {
+        auto source        = source_code{ u8" 123 " };
+        const auto tokens  = tokenize(source);
+        auto parser        = parser_t{ tokens };
+        const auto pattern = std::vector<token_pattern>{ { token_type::whitespace, u8" " },
+                                                         { token_type::integer, u8"123" },
+                                                         { token_type::whitespace, u8" " }
+
+        };
+        const auto matched = parser.match_and_consume(pattern, false);
+        expect(matched.has_value());
+        expect_tokens(pattern, matched.value());
+    };
+
+    "parser_t can match all token types"_test = [] {
+        auto source        = source_code{ u8" 1``;+id" };
+        const auto tokens  = tokenize(source);
+        auto parser        = parser_t{ tokens };
+        const auto pattern = std::vector<token_pattern>{
+            { token_type::whitespace, u8" " },     { token_type::integer, u8"1" },
+            { token_type::literal, u8"``" },       { token_type::semantic_scope_operator, u8";" },
+            { token_type::operator_token, u8"+" }, { token_type::identifier, u8"id" }
+
+        };
+        const auto matched = parser.match_and_consume(pattern, false);
+        expect(matched.has_value());
+        expect_tokens(pattern, matched.value());
+    };
+
+    "parser_t can match all token types in str litteret with whitespace"_test = [] {
+        auto source       = source_code{ u8" 1\n``\t ; +\tid " };
+        const auto tokens = tokenize(source);
+        auto parser       = parser_t{ tokens };
+        const auto pattern =
+            std::vector<token_pattern>{ { token_type::integer, u8"1" },
+                                        { token_type::literal, u8"``" },
+                                        { token_type::semantic_scope_operator, u8";" },
+                                        { token_type::operator_token, u8"+" },
+                                        { token_type::identifier, u8"id" }
+
+            };
+        const auto matched = parser.match_and_consume(pattern);
+        expect(matched.has_value());
+        expect_tokens(pattern, matched.value());
+    };
+
+    "parser_t matching can fail gracefully"_test = [] {
+        auto source        = source_code{ u8"123" };
+        const auto tokens  = tokenize(source);
+        auto parser        = parser_t{ tokens };
+        const auto pattern = std::vector<token_pattern>{ { token_type::literal, u8"123" } };
+        const auto matched = parser.match_and_consume(pattern);
+        expect(not matched.has_value());
+    };
+
+    "parser_t matching can fail gracefully (more complex)"_test = [] {
+        auto source       = source_code{ u8" 1\n``\t ; +\tid " };
+        const auto tokens = tokenize(source);
+        auto parser       = parser_t{ tokens };
+        const auto pattern =
+            std::vector<token_pattern>{ { token_type::integer, u8"1" },
+                                        { token_type::literal, u8"``" },
+                                        { token_type::semantic_scope_operator, u8"(" },
+                                        { token_type::operator_token, u8"+" },
+                                        { token_type::identifier, u8"id" }
+
+            };
+        // Wrong semantic scope operator ( not ;.
+        const auto matched = parser.match_and_consume(pattern);
+        expect(not matched.has_value());
+    };
+
+    "parser_t matching fail does not consume tokens"_test = [] {
+        auto source         = source_code{ u8"123" };
+        const auto tokens   = tokenize(source);
+        auto parser         = parser_t{ tokens };
+        const auto pattern1 = std::vector<token_pattern>{ { token_type::literal, u8"123" } };
+        const auto matched1 = parser.match_and_consume(pattern1);
+        expect(not matched1.has_value());
+
+        const auto pattern2 = std::vector<token_pattern>{ { token_type::integer, u8"123" } };
+        const auto matched2 = parser.match_and_consume(pattern2);
+        expect(matched2.has_value());
+        expect_tokens(pattern2, matched2.value());
+
+        const auto pattern3 = std::vector<token_pattern>{ { token_type::integer, u8"123" } };
+        const auto matched3 = parser.match_and_consume(pattern3);
+        expect(not matched3.has_value());
+    };
+
+    "parser_t matching fail does not consume tokens (more complex)"_test = [] {
+        auto source       = source_code{ u8" 1\n``\t ; +\tid " };
+        const auto tokens = tokenize(source);
+        auto parser       = parser_t{ tokens };
+        const auto pattern1 =
+            std::vector<token_pattern>{ { token_type::integer, u8"1" },
+                                        { token_type::literal, u8"``" },
+                                        { token_type::semantic_scope_operator, u8"(" },
+                                        { token_type::operator_token, u8"+" },
+                                        { token_type::identifier, u8"id" }
+
+            };
+        // Wrong semantic scope operator ( not ;.
+        const auto matched1 = parser.match_and_consume(pattern1);
+        expect(not matched1.has_value());
+
+        const auto pattern2 =
+            std::vector<token_pattern>{ { token_type::integer, u8"1" },
+                                        { token_type::literal, u8"``" },
+                                        { token_type::semantic_scope_operator, u8";" },
+                                        { token_type::operator_token, u8"+" },
+                                        { token_type::identifier, u8"id" }
+
+            };
+        const auto matched2 = parser.match_and_consume(pattern2);
+        expect(matched2.has_value());
+        expect_tokens(pattern2, matched2.value());
+
+        const auto pattern3 =
+            std::vector<token_pattern>{ { token_type::integer, u8"1" },
+                                        { token_type::literal, u8"``" },
+                                        { token_type::semantic_scope_operator, u8";" },
+                                        { token_type::operator_token, u8"+" },
+                                        { token_type::identifier, u8"id" }
+
+            };
+        const auto matched3 = parser.match_and_consume(pattern3);
+        expect(not matched3.has_value());
+    };
+}
