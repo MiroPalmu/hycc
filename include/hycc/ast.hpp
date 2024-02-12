b199340413ee8708c93f80738ce07bee54c0e4a1
Miro Palmu <miro.palmu@helsinki.fi>
2024-02-12 18:27:35 +0200

    feat(ast): add initial plumbing

---
+++ include/hycc/ast.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <utility>
+#include <vector>
+
+#include "hycc/tokenizer.hpp"
+
+namespace hycc {
+namespace ast {
+
+class identifier_node {};
+class function_argument_node {};
+class type_node {};
+class expression_node {};
+class class_decleration_node {};
+class function_decleration_node {};
+class namespace_decleration_node {};
+class data_decleration_node {};
+class decleration_parsing_node {};
+class scope_node {};
+class if_statement_node {};
+class for_loop_statement_node_ {};
+class break_statement_node {};
+class continue_statement_node {};
+class return_statement_node {};
+
+} // namespace ast
+} // namespace hycc
