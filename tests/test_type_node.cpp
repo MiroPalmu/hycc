1afcc5a93799df9e35a6fc954ba36738df7336e3
Miro Palmu <email@miropalmu.cc>
2024-02-16 12:49:12 +0200

    feat(ast): add function type support

--- tests/test_type_node.cpp
+++ tests/test_type_node.cpp
@@ -20,6 +20,7 @@ int main() {
         expect(not type.is_const());
         expect(not type.is_pointer());
         expect(type.is_regular_type());
+        expect(not type.is_function());
     };
 
     "type_node can detect syntax error"_test = [] {
@@ -41,6 +42,7 @@ int main() {
         expect(type.is_const());
         expect(not type.is_pointer());
         expect(type.is_regular_type());
+        expect(not type.is_function());
     };
 
     "type_node can detect pointer types"_test = [] {
@@ -53,6 +55,7 @@ int main() {
         expect(not type.is_const());
         expect(not type.is_regular_type());
         expect(type.is_pointer());
+        expect(not type.is_function());
         expect(type.pointed_type().is_regular_type());
     };
 
@@ -66,6 +69,7 @@ int main() {
         expect(type.is_const());
         expect(not type.is_regular_type());
         expect(type.is_pointer());
+        expect(not type.is_function());
         expect(type.pointed_type().is_regular_type());
     };
 
@@ -79,14 +83,17 @@ int main() {
         expect(not type.is_const());
         expect(not type.is_regular_type());
         expect(type.is_pointer());
+        expect(not type.is_function());
 
         expect(not type.pointed_type().is_regular_type());
         expect(type.pointed_type().is_pointer());
         expect(not type.pointed_type().is_const());
+        expect(not type.pointed_type().is_function());
 
         expect(type.pointed_type().pointed_type().is_regular_type());
         expect(not type.pointed_type().pointed_type().is_pointer());
         expect(not type.pointed_type().pointed_type().is_const());
+        expect(not type.pointed_type().is_function());
     };
 
     "type_node can detect pointer to const pointer types"_test = [] {
@@ -99,13 +106,109 @@ int main() {
         expect(not type.is_const());
         expect(not type.is_regular_type());
         expect(type.is_pointer());
+        expect(not type.is_function());
 
         expect(not type.pointed_type().is_regular_type());
         expect(type.pointed_type().is_pointer());
         expect(type.pointed_type().is_const());
+        expect(not type.pointed_type().is_function());
 
         expect(type.pointed_type().pointed_type().is_regular_type());
         expect(not type.pointed_type().pointed_type().is_pointer());
         expect(type.pointed_type().pointed_type().is_const());
+        expect(not type.pointed_type().pointed_type().is_function());
+    };
+
+    "type_node can detect function with no arguments"_test = [] {
+        auto source       = source_code(u8"() -> *int = ");
+        const auto tokens = tokenize(source);
+        auto parser       = parser_t{ tokens };
+        auto type         = ast::type_node{};
+        type.push(parser);
+
+        expect(type.is_function());
+        expect(not type.is_const());
+        expect(not type.is_regular_type());
+        expect(not type.is_pointer());
+
+        expect(type.function().args.get_args().empty());
+        expect(not type.function().return_type.is_regular_type());
+        expect(not type.function().return_type.is_const());
+        expect(type.function().return_type.is_pointer());
+        expect(not type.function().return_type.is_function());
+    };
+
+    "type_node can detect function with arguments"_test = [] {
+        auto source       = source_code(u8"(foo: int, move bar: float) -> *int = ");
+        const auto tokens = tokenize(source);
+        auto parser       = parser_t{ tokens };
+        auto type         = ast::type_node{};
+        type.push(parser);
+
+        expect(type.is_function());
+        expect(not type.is_const());
+        expect(not type.is_regular_type());
+        expect(not type.is_pointer());
+
+        expect(type.function().args.get_args().size() == 2);
+
+        expect(not type.function().return_type.is_regular_type());
+        expect(not type.function().return_type.is_const());
+        expect(type.function().return_type.is_pointer());
+        expect(not type.function().return_type.is_function());
+    };
+
+    "type_node can detect function which return function"_test = [] {
+        auto source       = source_code(u8"(foo) -> (bar) -> int = ");
+        const auto tokens = tokenize(source);
+        auto parser       = parser_t{ tokens };
+        auto type         = ast::type_node{};
+        type.push(parser);
+
+        expect(type.is_function());
+        expect(not type.is_const());
+        expect(not type.is_regular_type());
+        expect(not type.is_pointer());
+
+        expect(type.function().args.get_args().size() == 1);
+
+        expect(not type.function().return_type.is_regular_type());
+        expect(not type.function().return_type.is_const());
+        expect(not type.function().return_type.is_pointer());
+        expect(type.function().return_type.is_function());
+
+        expect(type.function().return_type.function().args.get_args().size() == 1);
+
+        expect(type.function().return_type.function().return_type.is_regular_type());
+        expect(not type.function().return_type.function().return_type.is_const());
+        expect(not type.function().return_type.function().return_type.is_pointer());
+        expect(not type.function().return_type.function().return_type.is_function());
+    };
+
+    "type_node can detect syntax error when no return type separator"_test = [] {
+        auto source       = source_code(u8"(foo: int, move bar: float) type = ");
+        const auto tokens = tokenize(source);
+        auto parser       = parser_t{ tokens };
+        auto type         = ast::type_node{};
+
+        expect(throws<syntax_error>([&] { type.push(parser); }));
+    };
+
+    "type_node can detect syntax error when return type separator contains whitespace"_test = [] {
+        auto source       = source_code(u8"(foo: int, move bar: float) - >  = ");
+        const auto tokens = tokenize(source);
+        auto parser       = parser_t{ tokens };
+        auto type         = ast::type_node{};
+
+        expect(throws<syntax_error>([&] { type.push(parser); }));
+    };
+
+    "type_node can detect syntax error when no return type"_test = [] {
+        auto source       = source_code(u8"(foo: int, move bar: float) ->  = ");
+        const auto tokens = tokenize(source);
+        auto parser       = parser_t{ tokens };
+        auto type         = ast::type_node{};
+
+        expect(throws<syntax_error>([&] { type.push(parser); }));
     };
 }
